28fe23adc01e5fc711a3fdd64f5e207de6386ccb
patelabhi23 <patelabhi2301@gmail.com>
2021-05-23 13:01:21 +0530

    abhi

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,55 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div id="game">
+      <h1>Tic Tac Toe</h1>
+      <div id="board">
+          <button class="cell">
+              <span class="cell-content"></span>
+          </button>
+          <button class="cell">
+              <span class="cell-content"></span>
+          </button>
+          <button class="cell">
+              <span class="cell-content"></span>
+          </button>
+          <button class="cell">
+              <span class="cell-content"></span>
+          </button>
+          <button class="cell">
+              <span class="cell-content"></span>
+          </button>
+          <button class="cell">
+              <span class="cell-content"></span>
+          </button>
+          <button class="cell">
+              <span class="cell-content"></span>
+          </button>
+          <button class="cell">
+              <span class="cell-content"></span>
+          </button>
+          <button class="cell">
+              <span class="cell-content"></span>
+          </button>
+      </div>
+  </div>
+
+  <div id="modal-overlay">
+      <div id="game-result-modal">
+          <div id="result-container">
+              <div id="winner-container">
+                  <span></span>
+              </div>
+          </div>
+          <div id="new-game-container">
+              <button id="new-game-button">Start New Game</button>
+          </div>
+      </div>
+  </div>
+    </>
+  );
+}
+
+export default App;
